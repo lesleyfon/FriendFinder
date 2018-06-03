ef4987d143d0a5ba791da4b17b0c744e0eb6385d
lesleyfon <lesleybanadzem2017@gmail.com>
2018-06-03 10:49:20 -0700

    installed the npm packages

---
+++ server.js
@@ -0,0 +1,7 @@
+// Your server.js file should require the basic npm packages we've used in class: express, body-parser and path.
+
+var express = require("express");
+var bodyparser = require("bodyparser");
+var path = require("path");
+
+var app = express()
