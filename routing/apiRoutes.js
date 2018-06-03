bee17f47c9c816b03d92de875369ca576dc30f8e
lesleyfon <lesleybanadzem2017@gmail.com>
2018-06-03 12:14:44 -0700

    updated the api routes file

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,2 +1,20 @@
-
+// We are linking our routes to a series of "data" sources.
 var friendsData = require("../app/data/friends");
+// API GET Requests
+// Below code handles when users "visit" a page.
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+    });
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // (ex. User fills out a reservation request... this data is then sent to the server...
+    // Then the server saves the data to the tableData array)
+
+    //A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+    app.post("/api/friends", function (req, res) {
+        // code goese here
+    });
+}
